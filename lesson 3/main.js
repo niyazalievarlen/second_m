29f4e8145ff260f37fbd2dba1c9806a2899b1f2a
niyazalievarlen <niyazalievarlen@gmail.com>
2022-04-13 21:54:25 +0600

    fhua

---
+++ lesson 3/main.js
@@ -0,0 +1,78 @@
+// function sayHello() {
+//     console.log("Hello")
+// }
+//
+// sayHello();
+//
+//
+// function getFullName(user) {
+//     console.log()
+// }
+//
+// const user = {
+//     firsr_name: "John",
+//     last_name: "Barbaro"
+// }
+//
+// getFullName(user,18);
+//
+// const user2 = {
+//     firsr_name: "Vito"
+//     last_name: "Vcalette"
+// }
+//
+// getFullName(user,15)
+// getFullName(user,19)
+
+
+const anonym =function (user) {
+    console.log(user.first_name,user.last_name)
+}
+
+
+let number1 = 10;
+let number2 = 15;
+number1 = 20;
+let number3 = number1 + number2;
+let number4 = number3 - 5;
+console.log(number1)
+console.log(number4)
+console.log(111111)
+
+
+function twoMass(arr1, arr2) {
+    if (arr1.length < arr2.length) {
+        console.log("Первый массив больше!")
+    } else {
+        console.log("Второй массив больше!")
+    }
+}
+let arr1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+    arr2 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12]
+twoMass(1, 2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
